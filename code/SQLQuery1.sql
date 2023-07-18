d93c4e95de605933ead4b37999952cb1f10ea1d5
Neura-Shadow <86242536+Neura-Shadow@users.noreply.github.com>
2023-07-18 10:59:19 +0800

    Update SQLQuery1.sql

--- code/SQLQuery1.sql
+++ code/SQLQuery1.sql
@@ -1,25 +1,81 @@
+Select *
+From SQLserver..CovidDeaths
+Where continent is not null 
+order by 3,4
+
+
+
 Select Location, date, total_cases, new_cases, total_deaths, population
-From Protfolio..CovidDeaths
+From SQLserver..CovidDeaths
 Where continent is not null 
 order by 1,2
 
 
-----SELECT Location,date,population,max(total_cases) as limit 
-----from CovidDeaths
-----group by location,date,population
-----order by limit desc
+
+Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From SQLserver..CovidDeaths
+Where location like '%states%'
+and continent is not null 
+order by 1,2
 
 
---select location,MAX(cast(total_deaths as int))as TotalDeathCount
---from CovidDeaths
---where continent is null
---group by location
---order by TotalDeathCount desc
+-- Total Cases vs Population
+
+Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
+From SQLserver..CovidDeaths
+--Where location like '%states%'
+order by 1,2
+
+
+-- Countries with Highest Infection Rate and Death compared to Population
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From SQLserver..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+Select continent,Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From SQLserver..CovidDeaths
+--Where location like '%states%'
+Where continent is not null 
+Group by Location
+order by TotalDeathCount desc
 
 
 
+-- Total Population vs Vaccinations
+	
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From SQLserver..CovidDeaths dea
+Join SQLserver..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+order by 2,3
 
--- Using Temp Table to perform Calculation on Partition By in previous query
+
+
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From SQLservere..CovidDeaths dea
+Join SQLserver..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+--order by 2,3
+)
+Select *, (RollingPeopleVaccinated/Population)*100
+From PopvsVac
+
+
 DROP Table if exists #PercentPopulationVaccinated
 Create Table #PercentPopulationVaccinated
 (
@@ -35,8 +91,8 @@ Insert into #PercentPopulationVaccinated
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
-From CovidDeaths dea
-Join CovidVaccinations vac
+From SQLserever..CovidDeaths dea
+Join SQLserver..CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 --where dea.continent is not null 
@@ -46,132 +102,24 @@ Select *, (RollingPeopleVaccinated/Population)*100
 From #PercentPopulationVaccinated
 
 
-
---perform Calculation on Partition By in previous query
-With popasvac(Continent,location,Date,Population,New_Vaccinations,RollingPeopleVaccinated)
-as
-(
-select dea.continent,dea.location, dea.date ,dea.population,vac.new_vaccinations,
-SUM(CONVERT(bigint,new_vaccinations)) over (partition by dea.location 
-ORDER BY dea.location,dea.date) as RollingpeopleVaccinated
-from CovidDeaths dea 
-join CovidVaccinations vac
-on dea.location = vac.location 
-and dea.date = vac.date
-where dea.continent is not null
---order by 2,3
-)
-select *
-from popasvac
-
-
-
 -- Creating View to store data for later visualizations
 
-create view pic as
-select dea.continent,dea.location, dea.date ,dea.population,vac.new_vaccinations,
-SUM(CONVERT(bigint,new_vaccinations)) over (partition by dea.location 
-ORDER BY dea.location,dea.date) as RollingpeopleVaccinated
-from CovidDeaths dea 
-join CovidVaccinations vac
-on dea.location = vac.location 
-and dea.date = vac.date
-where dea.continent is not null
---order by 2,3
-
----------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-Select *
-From PortfolioProject..NashvilleHousing
-order by ParcelID
-
-
-
-Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
-From PortfolioProject.dbo.NashvilleHousing a
-JOIN PortfolioProject.dbo.NashvilleHousing b
-	on a.ParcelID = b.ParcelID
-	AND a.[UniqueID ] <> b.[UniqueID ]
-Where a.PropertyAddress is null
-
-
-Update a
-SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
-From PortfolioProject.dbo.NashvilleHousing a
-JOIN PortfolioProject.dbo.NashvilleHousing b
-	on a.ParcelID = b.ParcelID
-	AND a.[UniqueID ] <> b.[UniqueID ]
-Where a.PropertyAddress is null
-
-
-
--- Breaking out Address into Individual Columns
-
-
-Select PropertyAddress
-From PortfolioProject.dbo.NashvilleHousing
---Where PropertyAddress is null
---order by ParcelID
-
-SELECT
-SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address
-, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as Address
-From PortfolioProject.dbo.NashvilleHousing
-
-
-ALTER TABLE NashvilleHousing
-Add PropertySplitAddress Nvarchar(255);
-Update NashvilleHousing
-SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
-
-
-ALTER TABLE NashvilleHousing
-Add PropertySplitCity Nvarchar(255);
-Update NashvilleHousing
-SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress))
-
-
-Select *
-From PortfolioProject.dbo.NashvilleHousing
+Create View PercentPopulationVaccinated as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From SQLserver..CovidDeaths dea
+Join SQLserver..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
 
 
 
 
 
-Select OwnerAddress
-From PortfolioProject.dbo.NashvilleHousing
+Select Location, date, total_cases, new_cases, total_deaths, population
+From SQLserver..CovidDeaths
+Where continent is not null 
+order by 1,2
 
-
-Select
-PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
-,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
-,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
-From PortfolioProject.dbo.NashvilleHousing
-
-
-
-ALTER TABLE NashvilleHousing
-Add OwnerSplitAddress Nvarchar(255);
-Update NashvilleHousing
-SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
-
-
-ALTER TABLE NashvilleHousing
-Add OwnerSplitCity Nvarchar(255);
-Update NashvilleHousing
-SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
-
-
-
-ALTER TABLE NashvilleHousing
-Add OwnerSplitState Nvarchar(255);
-Update NashvilleHousing
-SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
-
-
-
-Select *
-From PortfolioProject.dbo.NashvilleHousing
